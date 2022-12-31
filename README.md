2d01704c62c491bbaead0115e628c7606727faa3
Mamoru Hashimoto <you@example.com>
2022-12-31 21:29:07 +0900

    メッセージの返信処理を追加

---
+++ README.md
@@ -0,0 +1,27 @@
+1. initialize
+
+```
+$ npm init
+```
+
+2. push source
+
+```
+$ git init
+```
+
+3. run ngrok.
+
+```
+$ ngrok http 3000
+```
+
+4. set webhook url
+
+Webhook URL: https://a05f-61-25-23-68.ngrok.io/bot/webhook
+
+```
+{ destination: 'U13776ebf32be5eee178f5ef84786eb6b', events: [] }
+```
+
+eol
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 // -----------------------------------------------------------------------------
 // モジュールのインポート
+require('dotenv').config();
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
-require('dotenv').config();
-
-console.log(process.env);
 
 // -----------------------------------------------------------------------------
 // パラメータ設定
@@ -18,9 +16,39 @@ const line_config = {
 server.listen(process.env.PORT || 3000);
 
 
+// APIコールのためのクライアント・インスタンスを作成
+const bot = new line.Client(line_config);
+
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする．
     res.sendStatus(200);
-    console.log(req.body);
+    console.dir(req.body, { depth: 10, colors: true });
+
+    // すべてのイベント処理のプロミスを格納する配列．
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理．
+    req.body.events.forEach((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつテキストタイプだった場合
+        if (event.type == "message" && event.message.type == "text") {
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応．
+            if (event.message.text == "こんにちは") {
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加．
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }))
+            }
+        }
+
+        // すべてのイベント処理が終了したら何個のイベントが処理されたか出力．
+        Promise.all(events_processed).then(
+            (response) => {
+                console.log(`${response.length} event(s) processed.`);
+            }
+        );
+    });
 });
+
