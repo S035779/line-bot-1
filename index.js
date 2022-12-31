61bd65e9024fc1a781bb51b6c5f4c63e7ddb4118
Mamoru Hashimoto <you@example.com>
2022-12-31 19:40:05 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// -----------------------------------------------------------------------------
+// モジュールのインポート
+const server = require("express")();
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+require('dotenv').config();
+
+console.log(process.env);
+
+// -----------------------------------------------------------------------------
+// パラメータ設定
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+};
+
+// -----------------------------------------------------------------------------
+// Webサーバー設定
+server.listen(process.env.PORT || 3000);
+
+
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+});
